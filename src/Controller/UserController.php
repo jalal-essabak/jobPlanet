70e8a362451a477e98dd78ad3b5f501d159b36a4
khyassine <kh.yassine18@gmail.com>
2019-09-15 15:20:04 +0100

    register form validation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -27,22 +27,29 @@ class UserController extends AbstractController
      * @Route("/user/register", name="registration")
      */
     public function register(UserPasswordEncoderInterface $encoder,Request $request){
-        
         $user=new User();
+
         $entityManager = $this->getDoctrine()->getManager();
-        
         $user->setFirstName($request->get('firstName'));
         $user->setLastName($request->get('lastName'));
         $user->setEmail($request->get('email'));
         $plainPassword = $request->get('password');
 
+        $userDB = $this->getDoctrine()
+        ->getRepository('App\Entity\User')
+        ->findOneBy(array('email' => $user->getEmail()));
+
+        if($userDB==null){
         $encodedPassword = $encoder->encodePassword($user, $plainPassword);
         $user->setPassword($encodedPassword);
-
         $entityManager->persist($user);
         $entityManager->flush();
+        
+        return $this->redirectToRoute('index');
+        }
 
-         return $this->redirectToRoute('index');
+        else
+        return $this->render('user/register.html.twig',['error'=>true]);
 
         
     }
@@ -62,12 +69,6 @@ class UserController extends AbstractController
             return $this->render('user/login.html.twig',['error'=>true,'email'=>$userEmail]);
         }
         else{
-            /*if($encoder->isPasswordValid($user,$userPlainPassword)){
-                return $this->redirectToRoute('index');
-            }
-            else
-            return $this->redirectToRoute('login',['error',true,'email'=>$userEmail]);
-            */
             return $this->redirectToRoute('index');
         }
         return $this->render('mainfiles/test.html.twig',['result'=>$result,'email'=>$userEmail]);
