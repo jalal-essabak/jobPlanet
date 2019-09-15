f5e566b015ee86351c99b76d529d02fb762a3746
khyassine <kh.yassine18@gmail.com>
2019-09-15 04:03:26 +0100

    added user login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -24,8 +27,8 @@ class UserController extends AbstractController
      * @Route("/user/register", name="registration")
      */
     public function register(UserPasswordEncoderInterface $encoder,Request $request){
+        
         $user=new User();
-
         $entityManager = $this->getDoctrine()->getManager();
         
         $user->setFirstName($request->get('firstName'));
@@ -43,4 +46,31 @@ class UserController extends AbstractController
 
         
     }
+     /**
+     * @Route("/user/login", name="user-login")
+     */
+    public function login(UserPasswordEncoderInterface $encoder,Request $request){
+
+        $userEmail=$request->get('email');
+        $userPlainPassword = $request->get('password');
+
+        $user = $this->getDoctrine()
+        ->getRepository('App\Entity\User')
+        ->findOneBy(array('email' => $userEmail));
+
+        if($user==null || !$encoder->isPasswordValid($user,$userPlainPassword)){
+            return $this->render('user/login.html.twig',['error'=>true,'email'=>$userEmail]);
+        }
+        else{
+            /*if($encoder->isPasswordValid($user,$userPlainPassword)){
+                return $this->redirectToRoute('index');
+            }
+            else
+            return $this->redirectToRoute('login',['error',true,'email'=>$userEmail]);
+            */
+            return $this->redirectToRoute('index');
+        }
+        return $this->render('mainfiles/test.html.twig',['result'=>$result,'email'=>$userEmail]);
+    }
+
 }   
